3d43c0fc0eb07c17f1bf78b13f77566afe5fe314
Matt Wetmore <wetmore.matt@gmail.com>
2014-08-28 16:04:32 -0400

    Make it better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function grabComments(id, after, prevArray, cb) {
     function (response) {
       if (response && !response.error) {
         var latestArray = prevArray.concat(response.data);
-        if (response.paging.next) {
+        if (response.paging && response.paging.next) {
           grabComments(id, response.paging.cursors.after, latestArray, cb);
         } else {
           cb(latestArray);
@@ -61,17 +61,13 @@ function collapse(commentsArray) {
 
 function getCommentsFromID() {
   var id = document.getElementById('id-entry').value;
-  var container = document.getElementById('comments');
-  var json = {};
 
   var addData = callbacker(2, function(data) {
     console.log(data);
+    renderFromData(data);
   });
-  container.innerHTML = '';
-
+  
   grabPost(id, function(response) {
-    document.getElementById('author').innerHTML = response.from.name;
-    document.getElementById('question-message').innerHTML = response.message;
     addData({
       question: {
         from: response.from.name,
@@ -81,29 +77,50 @@ function getCommentsFromID() {
   });
 
   grabComments(id, '', [], function(arr) {
-    addData({});
-    _.each(collapse(arr), function(comment) {
-      var commentDiv = document.createElement('div');
-      commentDiv.classList.add('comment');
-
-      var nameDiv = document.createElement('div');
-      nameDiv.innerHTML = comment.from;
-      nameDiv.classList.add('name');
-
-      var commentsDiv = document.createElement('div');
-      _.each(comment.messages, function(message) {
-        var messageDiv = document.createElement('div');
-        messageDiv.innerHTML = message;
-        messageDiv.classList.add('message');
-        commentsDiv.appendChild(messageDiv);
-      });
-
-      commentDiv.appendChild(nameDiv);
-      commentDiv.appendChild(commentsDiv);
-
-      container.appendChild(commentDiv);
+    addData({
+      comments: collapse(arr)
     });
   });
 
   return false;
+}
+
+function renderFromData(data) {
+  var container = document.getElementById('comments');
+  var json = {};
+
+  container.innerHTML = '';
+
+  document.getElementById('author').innerHTML = data.question.from;
+  document.getElementById('question-message').innerHTML = data.question.message;
+
+  if (data.comments.length == 0) {
+    container.innerHTML = 'No comments';
+  }
+
+  _.each(data.comments, function(comment) {
+    var commentDiv = document.createElement('div');
+    commentDiv.classList.add('comment');
+
+    var nameDiv = document.createElement('div');
+    nameDiv.innerHTML = comment.from;
+    nameDiv.classList.add('name');
+
+    var commentsDiv = document.createElement('div');
+    _.each(comment.messages, function(message) {
+      var messageDiv = document.createElement('div');
+      messageDiv.innerHTML = message;
+      messageDiv.classList.add('message');
+      commentsDiv.appendChild(messageDiv);
+    });
+
+    commentDiv.appendChild(nameDiv);
+    commentDiv.appendChild(commentsDiv);
+
+    container.appendChild(commentDiv);
+  });
+}
+
+function showHowTo() {
+  
 }
